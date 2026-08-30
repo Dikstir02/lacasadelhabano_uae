926f9d4a430ea0da36a899c189bed285bd7c5991
Dikstir02 <128859869+Dikstir02@users.noreply.github.com>
2026-08-30 22:24:42 +0800

    Delete unused event entries from events-data.js
    Removed multiple event entries from events-data.js.

--- js/events-data.js
+++ js/events-data.js
@@ -19,26 +19,5 @@ window.LCDH_EVENTS = [
     "title": "GOLDEN HOUR GATHERING",
     "copy": "A relaxed evening of shared tables, bright cocktails and old Havana spirit.",
     "date": "UAE · BY INVITATION"
-  },
-  {
-    "image": "https://images.pexels.com/photos/28539666/pexels-photo-28539666.jpeg",
-    "category": "123",
-    "title": "132",
-    "copy": "123",
-    "date": "123"
-  },
-  {
-    "image": "https://cdn.pixabay.com/audio/2026/02/24/00-12-33-504_200x200.jpeg",
-    "category": "UPCOMING EVENT",
-    "title": "HABANOS NIGHT",
-    "copy": "123123123123sdasdasdasdasd",
-    "date": "DUBAI JBR"
-  },
-  {
-    "image": "https://scontent.ffjr8-1.fna.fbcdn.net/v/t39.30808-6/555621234_2234262170334798_445424687395176848_n.jpg?stp=dst-jpg_tt6&cstp=mx1920x1920&ctp=s1920x1920&_nc_cat=104&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=wVTYf9fP2EkQ7kNvwEXi7yI&_nc_oc=AdrL1wDbvFb-XZRIEctufHVdLoOEEH6t2CXy5TqmrZe8v3l4KAHc-iAlzf24fbM3MgovQSFKxeoLRNm8x7gURBVm&_nc_zt=23&_nc_ht=scontent.ffjr8-1.fna&_nc_gid=ccNNPXf9ym5cAqPFwAMPGg&_nc_ss=7b2a8&oh=00_AQGSBED-jBMwDMZH5N5L9wt5O3oKHBGn5BjA24NQQ7bZ2Q&oe=6A93D563",
-    "category": "123",
-    "title": "dextter",
-    "copy": "asddsads",
-    "date": "DUBAI JBR"
   }
 ];
